950eaea2a0f4adbcbc0921095945ecc392d93050
UJay <plmoknijb1234510@gmail.com>
2023-08-04 14:38:19 +0800

    feat: 建立動態路由

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ let server = app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send("<h1>Hello World</h1>");
 });
+app.get('/student', (req, res) => {
+    res.sendFile(__dirname + '/views/student-login.html');
+});
+app.get('/student/:roomID', (req, res) => {
+    res.sendFile(__dirname + '/views/student-classroom.html');
+});
+app.get('/TEACHer', (req, res) => {
+    res.sendFile(__dirname + '/views/teacher-login.html');
+});
+app.get('/TEACHer/:roomID', (req, res) => {
+    res.sendFile(__dirname + '/views/teacher-classroom.html');
+});
