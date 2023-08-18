a906143c3207a4e74aff289cac6cc7ad06e60385
Freddy <freddy.wei0810@gmail.com>
2023-08-19 03:39:22 +0800

    feat: 建立基礎 socket.io 功能 調整： 1. 改寫 server.js 中伺服器相關程式碼設定，例如 let 改為 const，用以表示不會再更動的變數。 2. 加入 socket.io 相關程式碼至 server.js 中。 3. 於 server.js 中，以 /student 與 /TEACHer 路由，分別提供 views/student-login.html 與 views/teacher-login.html。 4. 以 cdn 方式加入 socket.io 功能至 views/student-login.html、views/teacher-login.html 中。 5. 初步測試可成功連接 socket。 6. views/student-classroom.html、views/teacher-classroom.html 僅因格式改變而變動。

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-let express = require('express');
-let app = express();
-let port  = 3000;
-let server = app.listen(port, () => {
-    console.log(`listening on *:${port}`);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const port = 3000;
+
+
+app.get('/student', (req, res) => {
+    res.sendFile(__dirname + '/views/student-login.html');
 });
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>");
+app.get('/TEACHer', (req, res) => {
+    res.sendFile(__dirname + '/views/teacher-login.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('connected', { id: socket.id, message: 'connected' });
+});
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
